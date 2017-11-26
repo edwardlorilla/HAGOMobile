5a9b8715c4992e0bd678e0eeeb3053fd4b8a1b7b
Edward Lance Lorilla <edwardlorilla1986@gmail.com>
2017-11-27 00:38:27 +0800

    fix bug lists during load time

--- app/Repository.php
+++ app/Repository.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Repository extends Model
 {
     protected $fillable =[
-      'name', 'description', 'scientificName', 'latitude', 'longitude'
+      'name', 'description', 'scientificName', 'latitude', 'longitude', 'altitude'
     ];
 
     /**
@@ -16,6 +16,13 @@ class Repository extends Model
     public function photos()
     {
         return $this->belongsToMany(Photo::class);
+    }
+    /**
+     * Get the disovery associated with the given Repository
+     */
+    public function discoveries()
+    {
+        return $this->belongsToMany(Discovery::class);
     }
     /*
      * admin/user capture associated with
